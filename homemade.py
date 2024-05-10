1e0bf73c4ca682bfc954ecf27a703ab4afdab58a
Tanner Gladson <tannergladson@gmail.com>
2024-05-10 17:53:07 -0400

    prepped to add values to ChessRobertaEngine

--- homemade.py
+++ homemade.py
@@ -9,33 +9,35 @@ import chess
 from chess.engine import PlayResult, Limit
 import random
 from lib.engine_wrapper import MinimalEngine, MOVE
-from typing import Any
+from typing import Any, Optional, Union
 import logging
+import sys
+from lib.config import Configuration
+from lib import model
 
-
-# Use this logger variable to print messages to the console or log files.
-# logger.info("message") will always print "message" to the console or log file.
-# logger.debug("message") will only print "message" if verbose logging is enabled.
+OPTIONS_TYPE = dict[str, Any]
+COMMANDS_TYPE = list[str]
+MOVE = Union[chess.engine.PlayResult, list[chess.Move]]
 logger = logging.getLogger(__name__)
-
 
 class ExampleEngine(MinimalEngine):
     """An example engine that all homemade engines inherit."""
-
     pass
-
-
-# Bot names and ideas from tom7's excellent eloWorld video
 
 class ChessRobertaEngine(ExampleEngine):
     """Chess Roberta! But for now, get a random move."""
 
-    def search(self, board: chess.Board, *args: Any) -> PlayResult:
-        """Choose a random move."""
-        return PlayResult(random.choice(list(board.legal_moves)), None)
-
-class RandomMove(ExampleEngine):
-    """Get a random move."""
+    def __init__(self, commands: COMMANDS_TYPE, options: OPTIONS_TYPE, stderr: Optional[int],
+                draw_or_resign: Configuration, game: Optional[model.Game], **popen_args: str):
+        """Start Stockfish."""
+        super().__init__(commands, options, stderr, draw_or_resign, game, **popen_args)
+        
+        self.chess_roberta_driver = None
+        
+    # def search(self, board: chess.Board, game: model.Game, time_limit: chess.engine.Limit, ponder: bool, draw_offered: bool,
+    #         root_moves: MOVE) -> chess.engine.PlayResult:
+    #     """Get a move using Stockfish."""
+    #     return self.engine.play(board, time_limit)
 
     def search(self, board: chess.Board, *args: Any) -> PlayResult:
         """Choose a random move."""
